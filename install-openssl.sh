ef29a334a573983111fb791a0e462f65dfd5e456
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-02-24 12:51:07 +0000

    feat: implement install-openssl.sh up to end of configuring

--- install-openssl.sh
+++ install-openssl.sh
@@ -1,28 +1,68 @@
 #!/bin/bash
 
+# Prints a "- Complete" message indented by one tab stop.
+complete () {
+    printf "\t- Complete\n"
+}
+
+version="3.6.1"
+library="openssl-$version"
+
 # Always download to the Downloads folder.
-cd ~/Downloads
+user="$(whoami)"
+cd /home/$user/Downloads
+printf "Switched to %s\n\n" "$(pwd)"
+
+# Remove any existing copies of this version of the library to ensure we start from fresh.
+printf "\nRemoving any existing copies of %s...\n\n" $library
+rm -rf $library*
 
 # Download and extract openssl 3.6.1.
-wget -q --show-progress https://www.openssl.org/source/openssl-3.6.1.tar.gz
-tar -zxf openssl-3.6.1.tar.gz
+filename="$library.tar.gz"
+url="https://www.openssl.org/source/$filename"
+printf "Downloading %s from %s...\n" $library $url
+wget -q --show-progress $url
+
+printf "Extracting %s...\n" $filename
+tar -zxf $filename
+
+# Setup variables for log files.
+configure_log="/home/$user/Downloads/openssl-configure.log"
+make_log="/home/$user/Downloads/openssl-make.log"
+make_install_log="/home/$user/Downloads/openssl-make-install.log"
 
 # Compile openssl.
 cd openssl-3.6.1
-./config
-make
-sudo make install
+printf "\nSwitched to %s\n" "$(pwd)"
+printf "Configuring %s...\n" $library
 
-# Copy libraries for openssl to /usr/lib.
-sudo cp -r libssl.so.3 /usr/lib
-sudo cp -r libcrypto.so.3 /usr/lib
+./config &> $configure_log
+code=$(echo $?)
+if [[ $code -eq 0 ]]; then  # echo $? gives the return code of the most recent command - 0 for success.
+    printf "\t- Success!"
+else
+    printf "\nConfigure failed with code %d!\n" $code
+    exit
+fi
 
-# Update library links.
-sudo ldconfig /usr/lib
+# printf "\nMaking %s...\n" $library
+# make &> $make_install_log
 
-# Verify openssl installation
-openssl version
-which openssl
+# # TODO work out how to run sudo commands
+# sudo make install &> $make_install_log
+
+# # Copy libraries for openssl to /usr/lib.
+# sudo cp -r libssl.so.3 /usr/lib
+# sudo cp -r libcrypto.so.3 /usr/lib
+
+# # Update library links.
+# sudo ldconfig /usr/lib
+
+# # Verify openssl installation
+# openssl version
+# which openssl
 
 # Always return to the Downloads folder.
 cd ~/Downloads
+
+printf "\nDone!\n"
