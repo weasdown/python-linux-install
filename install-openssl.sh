371064b9c9feb15c606e1ed415de429ad27d4d1d
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-02-24 11:06:58 +0000

    feat: add prototype install-openssl.sh

--- install-openssl.sh
+++ install-openssl.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-# Placeholder for openssl install script.
+# Always download to the Downloads folder.
+cd ~/Downloads
+
+# Download and extract openssl 3.6.1.
+wget https://www.openssl.org/source/openssl-3.6.1.tar.gz
+tar -zxf openssl-3.6.1.tar.gz
+
+# Compile openssl.
+cd openssl-3.6.1
+./config
+make
+sudo make install
+
+# Copy libraries for openssl to /usr/lib.
+sudo cp -r libssl.so.3 /usr/lib
+sudo cp -r libcrypto.so.3 /usr/lib
+
+# Update library links.
+sudo ldconfig /usr/lib
+
+# Verify openssl installation
+openssl version
+which openssl
+
+# Always return to the Downloads folder.
+cd ~/Downloads
