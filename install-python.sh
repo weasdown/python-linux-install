a7ab1b76b5eca423f2ae78040200ebcff183d9dd
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-02-24 11:07:07 +0000

    feat: add prototype install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-# Placeholder for Python install script.
+# Always download to the Downloads folder.
+cd ~/Downloads
+
+# Download and extract Python 3.14.3.
+wget https://www.python.org/ftp/python/3.14.3/Python-3.14.3.tar.xz
+tar -xf Python-3.14.3.tar.xz
+
+# Compile Python
+cd Python-3.14.3
+./configure --enable-optimizations --with-openssl=/usr/local --with-ensurepip=install CFLAGS="-I/usr/include" LDFLAGS="-L/usr/lib"
+make
+make test
+sudo make altinstall
+
+# Verify Python installation
+python3.14 version
+which python3.14
+
+# Always return to the Downloads folder.
+cd ..
